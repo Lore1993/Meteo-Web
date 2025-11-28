d327d775ea626470afce6e860d5e9a4f77cf1d06
Lore1993 <lorenzo93pellegrini@gmail.com>
2025-11-28 16:32:39 +0100

    Done

--- src/components/MeteoCard.jsx
+++ src/components/MeteoCard.jsx
@@ -8,20 +8,20 @@ const MeteoCard = ({ weather }) => {
       className="card mx-auto p-4 sfondoCard"
     >
       <img
-        src="src/assets/city.png"
+        src="assets/city.png"
         alt="weather"
         style={{ width: "120px" }}
         className="mx-auto mb-3"
       />
 
-      <h2>{weather.city.name}</h2>
+      <h2>{weather.city.name} </h2>
+      <p><strong>Sea level:</strong> {info.main.sea_level ?? "N/A"}</p>
       <h3 className="text-muted">{info.weather[0].description}</h3>
-      <hr />
+     
 <div className="details">
       <p><strong>Temp min:</strong> {info.main.temp_min}°C</p>
       <p><strong>Temp max:</strong> {info.main.temp_max}°C</p>
       <p><strong>Percepita:</strong> {info.main.feels_like}°C</p>
-      <p><strong>Sea level:</strong> {info.main.sea_level ?? "N/A"}</p>
       </div>
     </div>
   );
